47cdc97b10d66946245c4083c1e8d6f928eb0cff
miturra580-source <miturra580@gmail.com>
2026-08-25 08:14:04 +0530

    Add Windows-compatible Auto Print Agent source

---
+++ agent/SunilAutoPrintAgent/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Net;
+using System.Windows.Forms;
+namespace SunilAutoPrintAgent {
+ static class Program {
+  [STAThread] static void Main() {
+   ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+   Application.EnableVisualStyles();
+   Application.SetCompatibleTextRenderingDefault(false);
+   Application.Run(new MainForm());
+  }
+ }
+}
